08387ec929c80f1c6672fd7b66242db3fa31b69d
sofiapoli-ship-it <sofia.poli@jetop.com>
2025-11-25 22:05:01 +0100

    aggiornamento

---
+++ mini-twitter/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+        <h1 className = "text-center text-4xl font bold flex items-center">
+          Welcome to mini Twitter
+        </h1>
+    </div>
+  );
+}
